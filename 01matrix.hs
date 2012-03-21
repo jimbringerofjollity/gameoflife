8059d1f616ec374a2631aba328cb5265d3577cf9
Jim Tao <myemailaint@gmail.com>
2012-03-21 04:11:23 -0400

    haskell ftw

--- 01matrix.hs
+++ 01matrix.hs
@@ -5,14 +5,14 @@ import Data.Char
 zeroOne :: Char -> Int
 zeroOne c = (ord c) `mod` 2
 
-binRow :: Int -> [Char] -> [Int]
-binRow n a = map zeroOne (take n a)
+binaryRow :: Int -> [Char] -> [Int]
+binaryRow n a = map zeroOne (take n a)
 
-binArray :: Int -> Int -> [Char] -> IO ()
-binArray m n a = (putStrLn . show) [binRow n (drop (i*n) a) | i <- [0..(m-1)]]
+binaryArray :: Int -> Int -> [Char] -> IO ()
+binaryArray m n a = (putStrLn . show) [binaryRow n (drop (i*n) a) | i <- [0..(m-1)]]
 
 generate :: [[Char]] -> IO ()
-generate a = getContents >>= binArray (read (a !! 0)) (read (a !! 1))
+generate a = getContents >>= binaryArray (read (a !! 0)) (read (a !! 1))
 
 main :: IO ()
 main = sequence_ [hSetEncoding stdin latin1, getArgs >>= generate]
